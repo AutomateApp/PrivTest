4c65d05a7fa2acdee907522bc0e5e6a22961dba4
AutomateApp <sebastian.lega@nagel-group.com>
2025-07-16 16:20:31 +0200

    Add files via upload

---
+++ env.js
@@ -0,0 +1,6 @@
+
+// UWAGA: NIE wrzucaj tego pliku do repozytorium!
+const GITHUB_TOKEN = "tu_wklej_token";
+const GITHUB_USER = "AutomateApp";
+const GITHUB_REPO = "SkanowanieHuby";
+const BRANCH = "main";
